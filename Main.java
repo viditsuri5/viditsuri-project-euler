7e28b8bbd7a603046441f74779e5812ec77c07c0
viditsuri5 <viditsuri5@gmail.com>
2025-01-10 23:07:55 +0000

    new changes

--- Main.java
+++ Main.java
@@ -2,5 +2,13 @@ public class SumOfMultiples {
     public static void main(String[] args){
         int limit = 100;
         int sum = 0;
+
+        for(int i = 0; i<limit; i++){
+            if (i % 3 ==0 || i % 5 == 0){
+                sum += i;
+            }
+        }
+
+        System.out.println("The sum of the multiples of 3 or 5 below 100 is: " + sum);
     }
 }
